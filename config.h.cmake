813d770fc68e460c050d5867112c1dfbf924534a
Hidehisa Akiyama <hidehisaakiyama@users.noreply.github.com>
2020-08-21 18:47:03 +0900

    config.h.cmakeを追加．.gitignoreの修正
    	modified:   .gitignore
    	new file:   config.h.cmake

---
+++ config.h.cmake
@@ -0,0 +1,22 @@
+
+#define PACKAGE_NAME "librcsc"
+
+#define VERSION @librcsc_VERSION@
+
+#cmakedefine HAVE_LIBZ
+
+#cmakedefine HAVE_WINDOWS_H
+
+#cmakedefine HAVE_ARPA_INET_H
+
+#cmakedefine HAVE_FCNTL_H
+
+#cmakedefine HAVE_NETINET_IN_H
+
+#cmakedefine HAVE_NETDB_H
+
+#cmakedefine HAVE_SYS_SOCKET_H
+
+#cmakedefine HAVE_SYS_TIME_H
+
+#cmakedefine HAVE_UNISTD_H
