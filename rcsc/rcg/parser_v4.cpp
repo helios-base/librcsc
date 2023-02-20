49874d061aeb545a5fd03bebaeebbc21128db5bf
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2023-02-20 15:01:17 +0900

    Add the parsing procedure for updating change_focus count to rcg::ParserV4.

--- rcsc/rcg/parser_v4.cpp
+++ rcsc/rcg/parser_v4.cpp
@@ -592,6 +592,12 @@ ParserV4::parseShow( const int n_line,
             p.tackle_count_ = static_cast< UInt16 >( std::strtol( buf, &next, 10 ) ); buf = next;
             p.pointto_count_ = static_cast< UInt16 >( std::strtol( buf, &next, 10 ) ); buf = next;
             p.attentionto_count_ = static_cast< UInt16 >( std::strtol( buf, &next, 10 ) ); buf = next;
+            while ( *buf == ' ' ) ++buf;
+            if ( *buf != ')' )
+            {
+                p.change_focus_count_ = static_cast< UInt16 >( std::strtol( buf, &next, 10 ) ); buf = next;
+            }
+
             while ( *buf == ')' ) ++buf;
             while ( *buf == ' ' ) ++buf;
 
