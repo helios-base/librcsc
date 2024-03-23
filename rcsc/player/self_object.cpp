9e634c878e35cf9946442b431110ffe1e74f5532
Hidehisa Akiyama <522630+hidehisaakiyama@users.noreply.github.com>
2024-03-24 00:04:39 +0900

    Support a bipedal dash model in v19 simulator (#67)
    * Implement two legs dash command parameters.
    
    * Implement two legs dash model.
    
    * Remove unused variables.
    
    * 互換性を保つために，古いActionEffector::getDashInfo()を復帰
    
    * Fix defects for estimating the effect of the two legs dash
    
    * Add condition to skip player type determination if player rotates by two legs dash model
    
    * Add debug messages for two legs dash model in SelfObject::update().

--- rcsc/player/self_object.cpp
+++ rcsc/player/self_object.cpp
@@ -211,7 +211,9 @@ SelfObject::update( const ActionEffector & act,
     M_pos_prev = M_pos;
 
     Vector2D accel( 0.0, 0.0 );
-    double dash_power = 0.0;
+    double dash_rotation = 0.0;
+    double left_dash_power = 0.0;
+    double right_dash_power = 0.0;
     double turn_moment = 0.0, turn_err = 0.0;
     double neck_moment = 0.0;
 
@@ -219,7 +221,8 @@ SelfObject::update( const ActionEffector & act,
     // base command
     switch ( act.lastBodyCommandType() ) {
     case PlayerCommand::DASH:
-        act.getDashInfo( &accel, &dash_power );
+        //act.getDashInfo( &accel, &dash_power );
+        act.getDashInfo( &accel, &dash_rotation, &left_dash_power, &right_dash_power );
         break;
     case PlayerCommand::TURN:
         act.getTurnInfo( &turn_moment, &turn_err );
@@ -274,9 +277,13 @@ SelfObject::update( const ActionEffector & act,
     }
 
     // stamina
-    M_stamina.simulateDash( playerType(), dash_power );
+    //M_stamina.simulateDash( playerType(), dash_power );
+    M_stamina.simulateDash( playerType(), left_dash_power, right_dash_power );
 
 #ifdef DEBUG_PRINT
+    dlog.addText( Logger::WORLD,
+                  __FILE__" (update) estimated turn=%.1f dash_rot=%.1f",
+                  turn_moment, dash_rotation );
     dlog.addText( Logger::WORLD,
                   __FILE__" (update) estimated stamina=%.1f effort=%f recovery=%f capacity=%.1f",
                   M_stamina.stamina(),
@@ -288,6 +295,7 @@ SelfObject::update( const ActionEffector & act,
     /////////////////////////////////////////////////////////////////
     // turn
     M_body += turn_moment;
+    M_body += dash_rotation;
 
     /////////////////////////////////////////////////////////////////
     // face
@@ -307,6 +315,12 @@ SelfObject::update( const ActionEffector & act,
     {
         M_pos += M_vel;
     }
+
+#ifdef DEBUG_PRINT
+    dlog.addText( Logger::WORLD,
+                  __FILE__" (update) estimated body=%.1f neck=%.1f face=%.1f",
+                  M_body.degree(), M_neck.degree(), M_face.degree() );
+#endif
 
     // rcssserver/src/object.C
     // PVector MPObject::noise()
