3e4799661a383217a1e8af9fce6864991874162a
Hidehisa Akiyama <hidehisa.akiyama@gmail.com>
2024-11-20 17:09:44 +0900

    (tentative) Add player type information.

--- src/rcg2csv.cpp
+++ src/rcg2csv.cpp
@@ -1,5 +1,29 @@
 // -*-c++-*-
 
+/*
+ *Copyright:
+
+ Copyright (C) Hidehisa AKIYAMA
+
+ This code is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public
+ License along with this library; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+ *EndCopyright:
+ */
+
+/////////////////////////////////////////////////////////////////////
+
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -283,8 +307,36 @@ CSVPrinter::handlePlayerParam( const rcsc::rcg::PlayerParamT & param )
 
 /*-------------------------------------------------------------------*/
 bool
-CSVPrinter::handlePlayerType( const rcsc::rcg::PlayerTypeT & )
+CSVPrinter::handlePlayerType( const rcsc::rcg::PlayerTypeT & ptype )
 {
+#if 0
+    static bool s_header = false;
+    if ( ! s_header )
+    {
+        M_os << "id,player_speed_max,stamina_inc_max,player_decay,inertia_moment,dash_power_rate,player_size,kickable_margin,kick_rand,extra_stamina,effort_max,effort_min,kick_power_rate,foul_detect_probability,catchable_area_l_stretch"
+             << '\n';
+        s_header = true;
+    }
+
+    M_os << ptype.id_
+         << ',' << ptype.player_speed_max_
+         << ',' << ptype.stamina_inc_max_
+         << ',' << ptype.player_decay_
+         << ',' << ptype.inertia_moment_
+         << ',' << ptype.dash_power_rate_
+         << ',' << ptype.player_size_
+         << ',' << ptype.kickable_margin_
+         << ',' << ptype.kick_rand_
+         << ',' << ptype.extra_stamina_
+         << ',' << ptype.effort_max_
+         << ',' << ptype.effort_min_
+         << ',' << ptype.kick_power_rate_
+         << ',' << ptype.foul_detect_probability_
+         << ',' << ptype.catchable_area_l_stretch_
+         << '\n';
+#else
+    (void)ptype;
+#endif
     return true;
 }
 
